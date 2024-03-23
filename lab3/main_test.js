a2e3b34cb461c6487f36fb182b63193090d405a7
Victor Tsai <princeoftennis735@gmail.com>
2024-03-23 22:13:03 +0800

    lab3 test case well done

--- lab3/main_test.js
+++ lab3/main_test.js
@@ -3,3 +3,80 @@ const assert = require('assert');
 const { Calculator } = require('./main');
 
 // TODO: write your tests here
+
+/**
+ * 測試 Calculator 的 exp 函數
+ */
+describe('Test function of Calculator exp(x) ',()=>{
+    // initialize Calculator class there
+    let calculator = new Calculator()
+
+    // test case 1
+    it('Parameter is not a number',()=> {
+        try{
+            let result = calculator.exp('string dummy')
+        }catch(error){
+            assert.equal('unsupported operand type',error.message)
+        }
+    })
+
+    // test case 2
+    it('number is overflow',()=> {
+        try{
+            let result = calculator.exp(1000000000)
+        }catch(error){
+            assert.equal('overflow',error.message)
+        }
+    })
+
+    // test case 3
+    it('return a number',()=> {
+        let result = calculator.exp(87)
+        console.log('result is '+result)
+        assert.equal(false, Number.isNaN(result))
+    })
+
+})
+
+/**
+ * 測試 Calculator 的 log 函數
+ */
+describe('Test function of Calculator log(x) ',()=>{
+    // initialize Calculator class there
+    let calculator = new Calculator()
+
+    // test case 1
+    it('Parameter is not a number',()=> {
+        try{
+            let result = calculator.log('string dummy')
+        }catch(error){
+            assert.equal('unsupported operand type',error.message)
+        }
+    })
+
+    // test case 2
+    it('number is infinity',()=> {
+        try{
+        let result = calculator.log(0)
+        }catch(error){
+            assert.equal('math domain error (1)',error.message)
+        }
+    })
+
+    // test case 3
+    it('is not a number ',()=> {
+        try{
+            let result = calculator.log(-10)
+        }catch(error){
+            assert.equal('math domain error (2)',error.message)
+        }
+    })
+
+    // test case 4
+    it('return a number',()=> {
+        let result = calculator.log(87)
+        console.log('result is '+result)
+        assert.equal(false, Number.isNaN(result))
+    })
+
+})
